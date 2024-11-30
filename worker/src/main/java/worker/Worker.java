09a401e575015e9fcaf32cb07024aa107e1af7dc
fakhri <fakhridarmawan@gmail.com>
2024-11-30 21:27:42 +0800

    change db connection

--- worker/src/main/java/worker/Worker.java
+++ worker/src/main/java/worker/Worker.java
@@ -68,11 +68,11 @@ class Worker {
     try {
 
       Class.forName("org.postgresql.Driver");
-      String url = "jdbc:postgresql://" + host + "/postgres";
+      String url = "jdbc:postgresql://" + host + "/demojenkinsack";
 
       while (conn == null) {
         try {
-          conn = DriverManager.getConnection(url, "postgres", "postgres");
+          conn = DriverManager.getConnection(url, "demoadmin", "demoadmin");
         } catch (SQLException e) {
           System.err.println("Waiting for db");
           sleep(1000);
